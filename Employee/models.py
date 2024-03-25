21b22632ae5c42e9f5df78d149cac18fc0d56952
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-03-25 19:55:34 +0545

    CRUD operations

--- Employee/models.py
+++ Employee/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+class Department(models.Model):
+    name = models.CharField(max_length = 150)
+    description = models.TextField(blank = True)
+    created_at = models.DateField()
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    name = models.CharField(max_length = 150)
+    email = models.EmailField()
+    phone_num = models.IntegerField(validators = [MaxValueValidator(9999999999), MinValueValidator(9)])
+    joined_date = models.DateField()
+    address = models.TextField()
+    department  = models.ForeignKey(Department, on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return self.name
