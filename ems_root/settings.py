f2cfa973a2533d01d99a469f51ad45b8acb60420
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-03-25 19:14:01 +0545

    db integration

--- ems_root/settings.py
+++ ems_root/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'ems_root.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ems_db",
+        "USER": "ems_admin",
+        "PASSWORD": "ems_db_123",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
