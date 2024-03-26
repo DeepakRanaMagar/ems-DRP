483bee39de616c2caab84b772c029efeebb5878f
DeepakRanaMagar <dpk.me.123@gmail.com>
2024-03-26 12:23:18 +0545

    .

--- Employee/apiviews.py
+++ Employee/apiviews.py
@@ -13,3 +13,25 @@ class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
 
 class EmployeeCreate(generics.CreateAPIView):
     serializer_class = EmployeeSerializer
+
+class DepartmentList(generics.ListAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+class DepartmentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+class DepartmentCreate(generics.CreateAPIView):
+    serializer_class = DepartmentSerializer
+
+class RoleList(generics.ListAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+class RoleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+class RoleCreate(generics.CreateAPIView):
+    serializer_class = RoleSerializer
--- Employee/urls.py
+++ Employee/urls.py
@@ -1,10 +1,24 @@
 
 from django.contrib import admin
 from django.urls import path
-from .apiviews import EmployeeList, EmployeeDetail, EmployeeCreate
+from .apiviews import EmployeeList, EmployeeDetail, EmployeeCreate, DepartmentList, DepartmentDetail, DepartmentCreate, RoleList, RoleDetail, RoleCreate
+from .views import index
+
 
 urlpatterns = [
+    path('', index),
+    # Employee paths
     path('employee/', EmployeeList.as_view(), name="EmployeeList"),
     path('employee/<int:pk>', EmployeeDetail.as_view(), name="EmployeeDetail"),
-    path('employe/create', EmployeeCreate.as_view(), name="CreateEmployee")
+    path('employe/create', EmployeeCreate.as_view(), name="CreateEmployee"),
+
+    # Department Paths
+    path('department/', DepartmentList.as_view(), name="DepartmentList"),
+    path('department/<int:pk>', DepartmentDetail.as_view(), name="DepartmentDetail"),
+    path('department/create', DepartmentCreate.as_view(), name="CreateDepartment"),
+
+    # Role Paths
+    path('role/', RoleList.as_view(), name="RoleList"),
+    path('role/<int:pk>', RoleDetail.as_view(), name="RoleDetail"),
+    path('role/create', RoleCreate.as_view(), name="CreateRole"),
 ]
